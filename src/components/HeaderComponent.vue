ec4fe9e697bf7e4ab888ba2f8c0ca957c2efd4ae
Onda-dev <manuelaltieri3@gmail.com>
2022-06-05 15:30:41 +0200

    Logo aggiunto

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header">
+        <div class="header__logo">
+            <img src="../assets/img/dc-logo.png" alt="dc logo">
+        </div>
+        <nav class="header__nav"></nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
