992007ea6d1dacdbb16c6152b1ef53dc167941ab
Onda-dev <manuelaltieri3@gmail.com>
2022-06-05 15:42:46 +0200

    Header inserito

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,9 +1,42 @@
 <template>
-    <header class="header">
+    <header class="header container">
         <div class="header__logo">
             <img src="../assets/img/dc-logo.png" alt="dc logo">
         </div>
-        <nav class="header__nav"></nav>
+        <nav class="header__nav">
+            <ul>
+                <li>
+                    <a href="#">characters</a>
+                </li>
+                <li>
+                    <a href="#">comics</a>
+                </li>
+                <li>
+                    <a href="#">movies</a>
+                </li>
+                <li>
+                    <a href="#">tv</a>
+                </li>
+                <li>
+                    <a href="#">games</a>
+                </li>
+                <li>
+                    <a href="#">collectibles</a>
+                </li>
+                <li>
+                    <a href="#">videos</a>
+                </li>
+                <li>
+                    <a href="#">fans</a>
+                </li>
+                <li>
+                    <a href="#">news</a>
+                </li>
+                <li>
+                    <a href="#">shop</a>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -14,5 +47,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1.25rem 0;
     
+    &__nav ul {
+        display: flex;
+        flex-direction: row;
+    }
+}
+
 </style>
